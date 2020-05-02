4c2021ea48aa580e7eb226c5250318a488ad0993
Devendra Singh <devsinghindra@gmail.com>
2020-05-02 20:48:08 +0530

    added card and position them with flex content need to be changed

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import styles from "./Projects.module.css";
+import Card from "../Card/Card";
 
 function Projects() {
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Some Projects made by me.</h1>
+      <div className={styles.card}>
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
