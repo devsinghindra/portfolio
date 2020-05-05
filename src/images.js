80a63033024d620cf3c9e8cea9cea670b3a45d15
Devendra Singh <devsinghindra@gmail.com>
2020-05-05 16:08:16 +0530

    added file for easily exporting images

---
+++ src/images.js
@@ -0,0 +1,64 @@
+import todo from "./assets/todo.png";
+import chatRoom from "./assets/chatRoom1.png";
+import covid19 from "./assets/covid19.png";
+import keeper from "./assets/keeper.png";
+import secrets from "./assets/secrets1.png";
+import flutterChat from "./assets/flutterChat1.jpg";
+import flutterTodo from "./assets/flutterTodo.jpg";
+import librarian from "./assets/librarian5.jpg";
+
+let dummy =
+  "Lorem ipsum dolor sit amet consectetur Animi consequuntur ad aspernatur! Quam sapiente quod adipisci excepturi accusantium aspernatur eius, culpa ipsa quasi dolorum deserunt, similique debitis necessitatibus facere rem!";
+
+const images = [
+  {
+    id: 1,
+    src: chatRoom,
+    title: "foo",
+    description: dummy,
+  },
+  {
+    id: 2,
+    src: todo,
+    title: "foo",
+    description: dummy,
+  },
+  {
+    id: 3,
+    src: covid19,
+    title: "foo",
+    description: dummy,
+  },
+  {
+    id: 4,
+    src: keeper,
+    title: "foo",
+    description: dummy,
+  },
+  {
+    id: 5,
+    src: secrets,
+    title: "foo",
+    description: dummy,
+  },
+  {
+    id: 6,
+    src: flutterChat,
+    title: "foo",
+    description: dummy,
+  },
+  {
+    id: 7,
+    src: flutterTodo,
+    title: "foo",
+    description: dummy,
+  },
+  {
+    id: 8,
+    src: librarian,
+    title: "foo",
+    description: dummy,
+  },
+];
+
+export default images;
