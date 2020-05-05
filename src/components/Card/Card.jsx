de8482dd30105049c7bf1024149c884ab471fd39
Devendra Singh <devsinghindra@gmail.com>
2020-05-05 16:07:18 +0530

    changed card styles and added code

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 import styles from "./Card.module.css";
-
-import proPic from "../../images/dreamPro.jpg";
+import ImageComponent from "./ImageComponent";
 
 function Card(props) {
   return (
     <div className={styles.container}>
-      <img src={proPic} alt="profile" />
-      {/* <div className={styles.content}> */}
-      <h2>Title</h2>
-      <p>
-        Contennt Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
-        sapiente in iure consectetur earum ullam harum eum! Molestiae odit
-        dolorem repellat in, quia obcaecati illum hic atque pariatur dolores
-        autem?
-      </p>
-      {/* </div> */}
+      {/* <img src={props.src} alt="project" /> */}
+      <ImageComponent src={props.src} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
     </div>
   );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import styles from "./Projects.module.css";
 import Card from "../Card/Card";
 
+import images from "../../images";
+
 function Projects() {
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>Some Projects made by me.</h1>
       <div className={styles.card}>
-        <Card />
-        <Card />
-        <Card />
+        {images.map((img, index) => {
+          return (
+            <Card
+              key={img.id}
+              src={img.src}
+              title={img.title}
+              description={img.description}
+            />
+          );
+        })}
       </div>
     </div>
   );
