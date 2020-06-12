2ff6d5c8d72e9b8a83d02bedc2e4dc46a6d383ff
Devendra Singh <devsinghindra@gmail.com>
2020-06-12 23:00:19 +0530

    removed background image and made nav bar more subtle

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,19 +4,13 @@ import styles from "./Home.module.css";
 function Home() {
   return (
     <div className={styles.bg}>
-      <h1 className={styles.title}>Myself Devendra Singh.</h1>
+      <h1 className={styles.title}>Hello there, my friend!.</h1>
+      <h1 className={styles.title}>Welcome to my portfolio website.</h1>
+      <h1 className={styles.title}>I am Devendra Singh.</h1>
+      <h1 className={styles.title}>(aka. devsinghindra)</h1>
       <p className={styles.content}>
         Currently pursuing B.Tech in Computer Science & Engineering at NIT
-        Patna. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id
-        dicta rerum distinctio enim quaerat. Voluptatibus recusandae consectetur
-        aperiam facilis id quas aliquid quam dolore, in optio, et sed labore
-        unde?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed qui
-        officiis dolores odio possimus sint quasi, consequuntur quas eaque
-        incidunt consequatur soluta quidem quam laboriosam quaerat cupiditate
-        quibusdam. Quam, ad? lorr Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Earum, qui cum dolor doloribus harum inventore eaque
-        excepturi eveniet iusto maiores minus dignissimos et! Quaerat ipsam
-        voluptatem veniam dolore itaque explicabo.
+        Patna.
       </p>
     </div>
   );
