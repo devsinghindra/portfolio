cb7634874b6605dfe1ebffff27084624c9fc6c5b
Devendra Singh <devsinghindra@gmail.com>
2020-12-21 14:49:06 +0530

    added service worker

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
+import * as serviceWorker from './serviceWorker';
 
 ReactDom.render(
     <React.StrictMode>
         <Router>
             <App />
         </Router>
-    </React.StrictMode>
-    , document.getElementById("root"));
+    </React.StrictMode>,
+    document.getElementById("root")
+);
+
+serviceWorker.register();
